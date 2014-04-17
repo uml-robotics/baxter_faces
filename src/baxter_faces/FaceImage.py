9ad4997a09bcd6958809a5e8bb00b25c3ddfd641
Mikhail Medvedev <mmedvede@cs.uml.edu>
2014-04-17 17:49:33 -0400

    Makes all the images available

--- src/baxter_faces/FaceImage.py
+++ src/baxter_faces/FaceImage.py
@@ -12,10 +12,15 @@ class FaceImage(object):
         self.images = {
             'indifferent': self._get_image('gerty_indifferent.png'),
             'happy': self._get_image('gerty_happy.png'),
+            'smile': self._get_image('gerty_goodnews.png'),
+            'excited': self._get_image('gerty_excited.png'),
+            'winking': self._get_image('gerty_winking.png'),
+            'surprised': self._get_image('gerty_surprised.png'),
             'thinking_left': self._get_image('gerty_thinking_left.png'),
             'thinking_right': self._get_image('gerty_thinking_right.png'),
             'confused': self._get_image('gerty_confused.png'),
             'unhappy': self._get_image('gerty_unhappy.png'),
+            'sad': self._get_image('gerty_sad.png'),
             'dead': self._get_image('gerty_dead.png'),
             'big_lemongrab': self._get_image('big_lemongrab.png'),
         }
